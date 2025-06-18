75df2b6fb1b3cfc7db1d554b0daaeb7f66d870c8
AGhibaudo <alanghib05@gmail.com>
2025-06-18 12:12:19 -0300

    Funciones para el calculo del Vector Estado

--- backend/app/utilities.py
+++ backend/app/utilities.py
@@ -1,74 +1,92 @@
+import math
 
-def funcionEDO(c, t, x):
+
+def funcion_edo(c, t, x):
     # dR/dt = C + 0,2T + t^2
     # t asume el valor de x en nuestra EDO.
     # el parametro t es nuestro T en la EDO.
     # Tanto C como T son constantes que se definen en la función.
     # En este caso, C es la cantidad de personas en la cola que debemos observar en el vec estado.
-    funcionValuada = c + 0,2*t + x**2
-    return funcionValuada
+    return c + 0,2*t + x**2
 
-class Cliente:
-    def __init__(self, id, tipo, estado, tiempoLlegada, inicioAtencion, finAtencion):
+
+def nom_servidor(tipo):
+    if tipo == 1:
+        return "Envio de Paquete"
+    return "Reclamaciones y Devoluciones"
+
+def distribucion_uniforme(valor_inf, valor_sup, rnd):
+    """
+    Acá el enunciado no nos indica nada sobre el si R cambia exactamente para cada tipo de servidor, lo que tenemos
+    es una distribución uniforme que va desde 100 a 300 que luego será utilizado para el calculo de Runge Kutta de 4to Orden.
+    """
+    return valor_inf + (rnd * (valor_sup - valor_inf))
+
+def distribucion_exp_neg(mu, rnd):
+    # mu = hace referencia a la media
+    # rnd = el valor aleatorio.
+    return -mu * math.log(1 - rnd)
+
+class Servidor:
+    def __init__(self, id, tipo, nombre, rnd, T, demora, hora_fin_atencion, estado):
         self.id = id
         self.tipo = tipo
+        self.nombre = (nombre == nom_servidor(tipo))
+        self.rnd = rnd # Falta poner la función para q me de un numero aleatorio acá jijo
+        self.T = distribucion_uniforme(rnd)
+        self.demora = demora # Falta hacer lo de runge kutta acá
+        self.hora_fin_atencion = hora_fin_atencion
         self.estado = estado
-        self.tiempoLlegada = tiempoLlegada
-        self.inicioAtencion = inicioAtencion
-        self.finAtencion = finAtencion
-
-class ServidorEnvio:
-    def __init__(self, id, rnd, tUniforme, demora, horaFinAtencion, estado):
+    
+class Cliente:
+    def __init__(self, id, tipo_sv, estado, tiempo_llegada, inicio_atencion, fin_atencion)
         self.id = id
-        self.rnd = rnd
-        self.tUniforme = tUniforme
-        self.demora = demora
-        self.horaFinAtencion = horaFinAtencion
+        self.tipo_sv = tipo_sv
         self.estado = estado
+        self.tiempo_llegada = tiempo_llegada
+        self.inicio_atencion = inicio_atencion
+        self.fin_atencion = fin_atencion
 
-class ServidorReclamos:
-    def __init__(self, id, rnd, tUniforme, demora, horaFinAtencion, estado):
-        self.id = id
-        self.rnd = rnd
-        self.tUniforme = tUniforme
-        self.demora = demora
-        self.horaFinAtencion = horaFinAtencion
-        self.estado = estado
 
-def rungeKutta(f, x0, y0, h, n):
+def hs_a_min(mu, hs):
     """
-    Runge-Kutta method for solving ordinary differential equations.
-    
-    Parameters:
-    f : function - la función definida por la EDO
-    x0 : float - valor de x
-    y0 : float - valor de y
-    h : float - tamaño del salto.
-    n : int - cant de saltos.
-    
-    devuelve:
-    Una lista de tuplas (x, y) representa la solución en cada paso
+    1 h -> 60 m
+    n clientes
     """
-    a, rnd, b, t = 0, 0, 0, 0
-    C = 0 # Cantidad de personas en la cola.
-    T = 0
-    R = a + rnd(b-a)
-    result = [(x0, y0)]
-    for i in range(n):
-        k1 = h * f()
-        k2 = h * f(x0 + h / 2, y0 + k1 / 2)
-        k3 = h * f(x0 + h / 2, y0 + k2 / 2)
-        k4 = h * f(x0 + h, y0 + k3)
-        
-        y_next = y0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        x_next = x0 + h
-        
-        result.append((x_next, y_next)) 
-        
-        x0, y0 = x_next, y_next
-    
-    return result
+    minutos = hs * 60
+    return minutos/mu
 
+
+
+def tipo_servicio(rnd):
+    """
+    Para el tipo de servicio se realizo una tabla de probabilidades ya que el enunciado indicaba que para Envio de Paquetes [1]
+    cada uno de los empleados tiene una tasa de servicio de 10 clientes p/ hora.
+    Mientras que para Reclamaciones y devoluciones, un solo empleado con una tasa de servicio de 7 clientes p/ hora.
+    """
+    if 0.742 <= rnd <= 1.00:
+        return 2
+    return 1
+
+def llegadaCliente(tipo_servicio, reloj, rnd):
+    """
+    Para la llegada del cliente necesitamos saber el valor actual del reloj, y un valor aleatorio para indicar cuando 
+    llegará este, todo esto en base al tipo de servicio que tiene. Los calculos de la media son calculados mediante una regla
+    de tres, ya que pasamos de Horas a Minutos :)
+    """
+    if tipo_servicio == 1: # Esto equivaldría a que estamos ante Envio de Paquetes
+        mu = 25
+        horas = 1
+        media_de_cliente_por_min = hs_a_min(mu, horas)
+        llegada = distribucion_exp_neg(media_de_cliente_por_min, rnd)
+        return reloj + llegada 
+    else:
+        mu = 15
+        horas = 1
+        media_de_cliente_por_min = hs_a_min(mu, horas)
+        llegada = distribucion_exp_neg(media_de_cliente_por_min, rnd)
+        return reloj + llegada       
+    
 
 
 def tiempoEntreLlegadas(t1, t2):
