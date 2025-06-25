ca91ff910b60b84bec69ebfd1756b93a040c686c
valpec <valuupecchio@gmail.com>
2025-06-25 20:34:26 -0300

    anda

--- frontend/src/components/CuadroCabecera.jsx
+++ frontend/src/components/CuadroCabecera.jsx
@@ -4,24 +4,24 @@ function CuadroCabecera() {
   return (
     <thead>
       <tr>
-        <th colSpan={4} className="sticky-col"></th>
+        <th colSpan={3} className="sticky-col"></th>
         <th colSpan={6} className="table-dark">SERVICIOS</th>
         <th colSpan={2}></th>
-        <th colSpan={10} className="table-success">SERVIDORES ENVIO DE PAQUETES</th>
-        <th colSpan={5} className="table-danger">SERVIDOR RECLAMOS Y DEVOLUCIONES</th>
+        <th colSpan={6} className="table-success">SERVIDORES ENVIO DE PAQUETES</th>
+        <th colSpan={3} className="table-danger">SERVIDOR RECLAMOS Y DEVOLUCIONES</th>
         <th colSpan={6} className="table-primary">VARIABLES PARA ESTADISTICAS</th>
 
       </tr>
 
       <tr>
-        <th colSpan={2} className="sticky-col"></th>
-        <th colSpan={2} className="table-primary sticky-col-3">NECESIDAD TIPO SERVICIO</th>
+        <th colSpan={3} className="sticky-col"></th>
+
         <th colSpan={3} className="table-success">LLEGADA DE CLIENTE A ENVIO DE PAQUETE</th>
         <th colSpan={3} className="table-danger">LLEGADA DE CLIENTE A RECLAMOS Y DEVOLUCIONES</th>
         <th colSpan={2}></th>
-        <th colSpan={5} className="table-dark">EMPLEADO 1</th>
-        <th colSpan={5} className="table-dark">EMPLEADO 2</th>
-        <th colSpan={5} className="table-dark">EMPLEADO 1</th>
+        <th colSpan={3} className="table-dark">EMPLEADO 1</th>
+        <th colSpan={3} className="table-dark">EMPLEADO 2</th>
+        <th colSpan={3} className="table-dark">EMPLEADO 1</th>
         <th colSpan={3} className="table-primary">SERVIDOR 1</th>
         <th colSpan={3} className="table-primary">SERVIDOR 2</th>
 
@@ -32,25 +32,32 @@ function CuadroCabecera() {
       <th className="table-warning sticky-col">IT</th>
         <th className="table-warning sticky-col">RELOJ</th>
         <th className="table-warning sticky-col-2">EVENTO</th>
+
         <th className="table-dark sticky-col-3">RND</th>
-        <th className="table-secondary sticky-col-4">SERVICIO</th>
+        <th className="table-secondary sticky-col-4">TMPO ENTRE LLEG</th>
+        <th className="table-secondary">PROX LLEG</th>
+
         <th className="table-dark">RND</th>
         <th className="table-secondary">TMPO ENTRE LLEG</th>
-        <th className="table-secondary">HORA DE PROX LLEG</th>
+        <th className="table-secondary">PROX LLEG</th>
+
+        <th className="table-success">COLA PAQ</th>
+        <th className="table-danger">COLA REC</th>
+
         <th className="table-dark">RND</th>
-        <th className="table-secondary">TMPO ENTRE LLEG</th>
-        <th className="table-secondary">HORA DE PROX LLEG</th>
-        <th className="table-success">COLA EDP</th>
-        <th className="table-danger">COLA RYD</th>
+        <th className="table-secondary">RK</th>
+        <th className="table-secondary">FIN AT</th>
+
         <th className="table-dark">RND</th>
-        <th className="table-secondary">VARIABLE T</th>
-        <th className="table-secondary">DEMORA DE ATENCION</th>
-        <th className="table-secondary">HORA FIN DE ATENCION</th>
+        <th className="table-secondary">RK</th>
+        <th className="table-secondary">FIN AT</th>
+
+        <th className="table-dark">RND</th>
+        <th className="table-secondary">RK</th>
+        <th className="table-secondary">FIN AT</th>
+
         <th className="table-secondary">ESTADO</th>
-        <th className="table-dark">RND</th>
-        <th className="table-secondary">VARIABLE T</th>
-        <th className="table-secondary">DEMORA DE ATENCION</th>
-        <th className="table-secondary">HORA FIN DE ATENCION</th>
+        <th className="table-secondary">ESTADO</th>
         <th className="table-secondary">ESTADO</th>
         <th className="table-dark">RND</th>
         <th className="table-secondary">VARIABLE T</th>
